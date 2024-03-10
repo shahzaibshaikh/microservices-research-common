05bf6716e75abceb3f035ef4b9f9ac3e00749b7b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-03-09 19:44:40 -0500

    Updates

--- src/events/config.js
+++ src/events/config.js
@@ -1,41 +1,54 @@
-const { Kafka } = require("kafkajs");
+const { Kafka } = require('kafkajs');
+
+const RETRY_ATTEMPTS = 3; // Consider moving this to a config file
+const RETRY_DELAY = 1000; // Consider moving this to a config file
 
 class KafkaConfig {
   constructor() {
     this.kafka = new Kafka({
-      clientId: "research-bookstore-order",
-      brokers: ["kafka-service:9092"]
+      clientId: 'research-bookstore',
+      brokers: ['kafka-service:9092'],
     });
     this.producer = this.kafka.producer();
-    this.consumer = this.kafka.consumer({ groupId: "test-group" });
+    this.consumer = this.kafka.consumer({ groupId: 'test-group' });
   }
 
   async produce(topic, messages) {
-    try {
-      await this.producer.connect();
-      await this.producer.send({
-        topic: topic,
-        messages: messages
-      });
-    } catch (error) {
-      console.error(error);
-    } finally {
-      await this.producer.disconnect();
+    let attempts = 0;
+    while (attempts < RETRY_ATTEMPTS) {
+      try {
+        await this.producer.connect();
+        await this.producer.send({
+          topic,
+          messages,
+        });
+        console.log(`Message sent to topic: ${topic}`);
+        return; // Exit loop on successful send
+      } catch (error) {
+        attempts++;
+        console.warn(`Retrying message sending (attempt ${attempts}/${RETRY_ATTEMPTS})`);
+        await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
+      } finally {
+        await this.producer.disconnect();
+      }
     }
+    console.error('Failed to send message after retries');
   }
 
   async consume(topic, callback) {
     try {
       await this.consumer.connect();
-      await this.consumer.subscribe({ topic: topic, fromBeginning: true });
-    await this.consumer.run({
+      await this.consumer.subscribe({ topic, fromBeginning: true });
+      await this.consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
           const value = message.value.toString();
           callback(value);
-        }
+        },
       });
     } catch (error) {
-      console.error(error);
+      console.error('Error consuming message:', error);
+    } finally {
+      await this.consumer.disconnect();
     }
   }
 }
